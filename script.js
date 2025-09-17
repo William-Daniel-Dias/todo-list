a83849a20f6837a7b45ff0ce4c587b35a9a4f33c
William Daniel Dias <daniel.willia@hotmail.com>
2025-09-17 19:37:58 -0300

    incluindo o css

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-//const list = document.querySelector('#todo-list')
-
-//const li = document.createElement('li')
-
-//li.textContent = "Estudar com os materiais do classRoom"
-//list.append(li)
-
-//list.addEventListener('click', event => {
-// const li = event.target.closest('li')
-
-// if(!li) return
-
-//li.classList.toggle('done')
-//})
-
-
-
 const form = document.querySelector("#todo-form")
 const input = document.querySelector("#title")
 const list = document.querySelector("#todo-list")
